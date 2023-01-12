5e58ca7cabbecbf6b72131e13733352537b52a64
Satansgriller <maetthe_hollenstein@hotmail.com>
2023-01-12 14:06:32 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const content = document.getElementById('content');
+
+function fetchData(path) {
+    return fetch(path,
+        {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+            },
+        })
+        .then(response => response.json())
+
+}
+
+function initSlider(){
+    let products = fetchData('./fixtures/products.json');
+    products.then(data => {
+        for(let i=0; i < data.length; i++){
+
+            const product = document.createElement('div');
+            product.setAttribute('name', data[i].productName)
+            product.setAttribute('class', 'product')
+
+            const productImg = document.createElement('img');
+            productImg.src = data[i].backgroundSrc;
+
+
+            product.appendChild(productImg);
+            content.appendChild(product);
+        }
+    })
+}
+
+initSlider();
