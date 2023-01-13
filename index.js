b9cf8d3394ab63604fb6afed57838390f7ddfcd5
Satansgriller <maetthe_hollenstein@hotmail.com>
2023-01-13 13:46:15 +0100

    add slider

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
 const content = document.getElementById('content');
 
 function fetchData(path) {
-    return fetch(
-        path,
-        {
-            method: 'POST', // *GET, POST, PUT, DELETE, etc.
-            mode: 'cors', // no-cors, *cors, same-origin
-            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: 'same-origin', // include, *same-origin, omit
-            headers: {
-                'Accept': 'application/json',
-            },
-        })
+    return fetch(path, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Accept': 'application/json',
+        },
+    })
         .then(response => response.json())
 
 }
 
 function initSlider() {
     let products = fetchData('./fixtures/products.json');
+
+    const slideButtonContainer = document.createElement('div');
+    slideButtonContainer.setAttribute('id', 'slide-container');
+
+    const slideButtonForward = document.createElement('button');
+    slideButtonForward.setAttribute('class', 'slide-button');
+    slideButtonForward.addEventListener('click', () => {
+        slide('forward');
+    })
+
+    const slideButtonBackward = document.createElement('button');
+    slideButtonBackward.setAttribute('class', 'slide-button');
+    slideButtonBackward.addEventListener('click', () => {
+        slide('backward');
+    })
+
+    slideButtonContainer.appendChild(slideButtonForward);
+    slideButtonContainer.appendChild(slideButtonBackward);
+
     products.then(data => {
         for (let i = 0; i < data.length; i++) {
 
             const product = document.createElement('div');
-            product.setAttribute('name', data[i].productName)
-            product.setAttribute('id', 'product')
+            product.setAttribute('name', 'product')
+            product.setAttribute('id', 'product_' + data[i].pk.toString())
             product.setAttribute('class', 'product')
             product.style.backgroundImage = "url('" + data[i].backgroundSrc + "')";
 
@@ -49,7 +66,7 @@ function initSlider() {
             clickPosCPU.style.backgroundColor = "rgba(0, 255, 0, 0.5)";
             clickPosCPU.addEventListener('click', () => {
                 onComponentClick(2, data[i])
-            })
+            });
 
 
             const clickPosSSD = document.createElement('div');
@@ -62,47 +79,97 @@ function initSlider() {
             clickPosSSD.style.backgroundColor = "rgba(150, 150, 150, 0.7)";
             clickPosSSD.addEventListener('click', () => {
                 onComponentClick(3, data[i])
-            })
-
+            });
 
             content.appendChild(product);
             product.appendChild(clickPosGpu);
             product.appendChild(clickPosCPU);
             product.appendChild(clickPosSSD);
+            content.appendChild(slideButtonContainer);
 
-            break;
+            if (i > 0) {
+                product.hidden = true;
+            }
+
+            //break;
         }
     })
+}
+
+function slide(direction) {
+    var actProduct = null;
+    var newProduct = null;
+
+    var products = document.getElementsByName('product');
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].hidden === false) {
+            actProduct = products[i];
+        }
+    }
+
+    if (direction === 'forward') {
+
+        if (actProduct.id === 'product_1') {
+            newProduct = document.getElementById('product_2');
+            actProduct.hidden = true;
+            newProduct.hidden = false;
+        } else if (actProduct.id === 'product_2') {
+            newProduct = document.getElementById('product_3');
+            actProduct.hidden = true;
+            newProduct.hidden = false;
+        } else if (actProduct.id === 'product_3') {
+            newProduct = document.getElementById('product_1');
+            actProduct.hidden = true;
+            newProduct.hidden = false;
+        }
+    } else {
+        if (actProduct.id === 'product_1') {
+            newProduct = document.getElementById('product_3');
+            actProduct.hidden = true;
+            newProduct.hidden = false;
+        } else if (actProduct.id === 'product_2') {
+            newProduct = document.getElementById('product_1');
+            actProduct.hidden = true;
+            newProduct.hidden = false;
+        } else if (actProduct.id === 'product_3') {
+            newProduct = document.getElementById('product_2');
+            actProduct.hidden = true;
+            newProduct.hidden = false;
+        }
+    }
 }
 
 function onComponentClick(componentType, data) {
 
     if (componentType === 1) {
-        generateSpecificationDiv(1, data.canvas.gpu);
+        generateSpecificationDiv(1, data);
     } else if (componentType === 2) {
-        generateSpecificationDiv(2, data.canvas.cpu);
-    } else {
-        generateSpecificationDiv(3, data.canvas.ssd);
+        generateSpecificationDiv(2, data);
+    } else if (componentType === 3) {
+        generateSpecificationDiv(3, data);
     }
 }
 
 function generateSpecificationDiv(componentType, data) {
 
-    let product = document.getElementById('product');
+    let product = document.getElementById('product_' + data.pk.toString());
     product.hidden = true;
 
     let specificationDiv = document.createElement('div');
     specificationDiv.setAttribute('id', 'specificationDiv');
     specificationDiv.setAttribute('class', 'specification');
-    specificationDiv.style.backgroundImage = "url('" + data.imgSrc + "')";
+
 
     let specificationDescription = document.createElement('div');
     specificationDescription.style.width = "200px"
     specificationDescription.style.height = "300px"
     specificationDescription.style.backgroundColor = "rgba(255, 255, 255, 0.5)"
 
+    content.appendChild(specificationDiv);
 
     if (componentType === 1) {
+
+        specificationDiv.style.backgroundImage = "url('" + data.canvas.gpu.imgSrc + "')";
 
         var image = document.getElementById('fanImg');
 
@@ -118,25 +185,24 @@ function generateSpecificationDiv(componentType, data) {
         var ctx = fanCanvas.getContext('2d');
         ctx.clearRect(0, 0, fanCanvas.width, fanCanvas.height);
         ctx.save();
-        ctx.drawImage(image,
-            fanCanvas.width / 2 - image.width / 2,
-            fanCanvas.height / 2 - image.height / 2
-        );
-        setTimeout(rotate, 2000)
+        ctx.drawImage(image, fanCanvas.width / 2 - image.width / 2, fanCanvas.height / 2 - image.height / 2);
+        setTimeout(rotateGpuFan, 2000)
+        specificationDiv.appendChild(fanCanvas)
 
+    } else if (componentType === 2) {
+        specificationDiv.style.backgroundImage = "url('" + data.canvas.cpu.imgSrc + "')";
+    } else if (componentType === 3) {
+        specificationDiv.style.backgroundImage = "url('" + data.canvas.ssd.imgSrc + "')";
     }
-
-    content.appendChild(specificationDiv);
-    specificationDiv.appendChild(fanCanvas)
     specificationDiv.appendChild(specificationDescription);
 }
 
 
-function rotate() {
+function rotateGpuFan() {
 
     const canvas = document.getElementById('canvas');
     var image = document.getElementById('fanImg');
-    image.src = '../media/img/GPU_1_fan.png';
+    image.src = 'media/img/GPU_1_fan.png';
     const ctx = canvas.getContext('2d');
 
 
@@ -146,13 +212,9 @@ function rotate() {
     ctx.translate(-150, -75);
 
 // Rotated rectangle
-    ctx.drawImage(image,
-        canvas.width / 2 - image.width / 2,
-        canvas.height / 2 - image.height / 2
-    );
+    ctx.drawImage(image, canvas.width / 2 - image.width / 2, canvas.height / 2 - image.height / 2);
 
-
-    setTimeout(rotate, 15)
+    setTimeout(rotateGpuFan, 15)
 }
 
 initSlider();
